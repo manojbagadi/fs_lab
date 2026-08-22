f5f1cafcb406a66e3960e9b1e5497d67510af68b
manojbagadi <manojbagadi77@gmail.com>
2026-08-22 20:13:37 +0530

    observation

---
+++ node modules/app.js
@@ -0,0 +1,6 @@
+const math = require("./math");
+
+console.log("Addition:", math.add(20, 10));
+console.log("Subtraction:", math.subtract(20, 10));
+console.log("Multiplication:", math.multiply(20, 10));
+console.log("Division:", math.divide(20, 10));
